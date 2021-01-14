35a88909aa2f003f198b4e429681ce72b0eefc64
horizon <1713113814@qq.com>
2021-01-14 10:13:40 +0800

    open the program successfully!

---
+++ src/components/student/student_home.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "student_home"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'; /* 引入axios进行地址访问*/
+Vue.use(ElementUI);
+Vue.config.productionTip = false;
+// 挂载到vue原型链上
+Vue.prototype.axios = axios;
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
